9416609162bc5b922c6e884cdb29e37ef6269de7
Tommy Salomonsson <tommy@salomonsson.se>
2011-10-22 14:25:32 +0200

    added a colorRect component and colorRect system... kind of hackish, just for first test

---
+++ src/se/anyro/tewonderhack/subsystem/RenderColorRectsSystem.java
@@ -0,0 +1,41 @@
+package se.anyro.tewonderhack.subsystem;
+
+import java.util.List;
+
+import se.anyro.tewonderhack.GameView;
+import se.anyro.tewonderhack.components.RenderColorRect;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import com.wikidot.entitysystems.rdbmsbeta.EntityManager;
+import com.wikidot.entitysystems.rdbmsbeta.SubSystem;
+
+public class RenderColorRectsSystem implements SubSystem {
+
+	private EntityManager em;
+	private GameView view;
+
+	public RenderColorRectsSystem(EntityManager em, GameView view) {
+		this.em = em;
+		this.view = view;
+	}
+
+	@Override
+	public void processOneGameTick(long lastFrameTime) {
+
+		List<RenderColorRect> renderables = em.getAllComponentsOfType(RenderColorRect.class);
+
+		if (!view.available)
+			return;
+
+		Paint paint = new Paint();
+		paint.setColor(0xff0000);
+		Canvas canvas = view.holder.lockCanvas();
+
+		for (RenderColorRect comp : renderables) {
+			canvas.drawColor(0x000000);
+			canvas.drawRect(comp.x, comp.y, (comp.x + comp.width), (comp.y + comp.height), paint);
+		}
+
+	}
+}
